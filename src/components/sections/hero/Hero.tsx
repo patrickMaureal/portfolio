37fc02c6a64e1e7a5396662007acbfa34bc66cd5
patrickMaureal <janpatrickmaureal@gmail.com>
2024-07-05 16:07:59 +0800

    update hero section

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -2,7 +2,10 @@ import myProfile from '../../../assets/profile/profile.png'
 const Hero = () => {
 	return (
 		<div className='flex flex-row justify-between items-center px-80'>
-			<h1>Hero</h1>
+			<div>
+				<h1 className='text-6xl font-bold'>Hi, I'm <span className='text-white'>Patrick</span></h1>
+				<p className='text-3xl'>I'm a <span className='text-white'>Web</span> developer</p>
+			</div>
 			<img src={myProfile} alt="" />
 		</div>
 	)
