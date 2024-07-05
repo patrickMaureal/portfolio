a5be7290637ad5691548a527b7e8706fc3cc8251
patrickMaureal <janpatrickmaureal@gmail.com>
2024-07-05 14:38:43 +0800

    update the navbar for mobile view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,43 @@
-import { HamburgerMenuIcon } from "@radix-ui/react-icons"
-import { useState } from "react"
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { useRecoilState } from "recoil";
+import { mobileMenuState } from "../state/atoms/mobileMenuState";
 
 const Navbar = () => {
-	const [isOpen, setIsOpen] = useState(false)
+  const [isMobileMenuOpen, setMobileMenuOpen] = useRecoilState(mobileMenuState);
 
-	return (
-		<header>
-			<nav className="flex justify-between items-center h-16 py-2 px-10 text-white m-4 text-lg font-bold">
-				<div className="hidden lg:block">
-					<h1>Navbar</h1>
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
-					<ul className="flex">
-						<li className="p-6">Home</li>
-						<li className="p-6">About</li>
-						<li className="p-6">Contact</li>
-					</ul>
+  return (
+    <header>
+      <nav className="flex justify-between items-center h-16 py-2 px-10 text-white m-4 text-lg font-bold relative">
+        <h1>Navbar</h1>
 
-					<button className="border p-3 rounded-3xl border-white hover:bg-white hover:text-black">Go to Projects</button>
-				</div>
-				
+        <div className="hidden lg:block">
+          <ul className="flex">
+            <li className="p-6">Home</li>
+            <li className="p-6">About</li>
+            <li className="p-6">Contact</li>
+          </ul>
+        </div>
 
-				<button>
-					<HamburgerMenuIcon height={24} width={24}/>
-				</button>
-			</nav>
-		</header>
-	)
-}
+        <div className="lg:hidden">
+          <button onClick={toggleMobileMenu}>
+            <HamburgerMenuIcon height={24} width={24} />
+          </button>
+        </div>
+      </nav>
+      
+      <div className={`lg:hidden absolute z-50 h-full w-full transform transition-transform duration-300 bg-gray-400 backdrop-filter backdrop-blur-md bg-opacity-20 ${isMobileMenuOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+        <ul className="flex flex-col justify-center items-center">
+          <li className="p-6 text-2xl">Home</li>
+          <li className="p-6 text-2xl">About</li>
+          <li className="p-6 text-2xl">Contact</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
