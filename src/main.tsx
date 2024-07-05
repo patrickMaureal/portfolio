5efbdb38bc2e32be8050736177e5bebc2b283b46
patrickMaureal <janpatrickmaureal@gmail.com>
2024-07-05 16:56:10 +0800

    add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { RecoilRoot } from 'recoil'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
 		<RecoilRoot>
+		<ChakraProvider theme={theme}>
 			<App />
+		</ChakraProvider>
 		</RecoilRoot>
   </React.StrictMode>,
 )
