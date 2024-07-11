d383f3e1757cbc4ec384916a2946f7542d049a16
patrickMaureal <janpatrickmaureal@gmail.com>
2024-07-11 17:10:38 +0800

    add responsiveness to hero

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -2,14 +2,14 @@ import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
 import myProfile from '../../../assets/profile/profile.png'
 const Hero = () => {
 	return (
-		<div className='flex justify-between items-center px-80 py-24 space-x-52'>
+		<div className='flex flex-col md:flex-row justify-between items-center p-8 md:px-40 md:py-12 lg:px-80 lg:py-24 space-y-20 lg:space-x-52'>
 			<div>
 				<h1 className='text-7xl font-bebas text-white'>Hi, I'm </h1>
 				<h1 className='text-7xl font-bebas text-white'>Patrick Maureal</h1>
 				<p className='text-lg text-subtitle'>A Sydney based front-end developer passionate about building accessible and user friendly websites.</p>
-				<div className='flex items-center space-x-4'>
-					<button className='mt-8 rounded-full bg-primary py-4 px-8'>
-						<span className='font-manrope font-bold'>DOWNLOAD RESUME</span>
+				<div className='flex items-center space-x-2 md:space-x-4'>
+					<button className='mt-8 rounded-full bg-primary py-4 px-4 md:py-4 md:px-8 '>
+						<span className='font-manrope font-extrabold md:font-bold text-sm md:text-xl'>DOWNLOAD RESUME</span>
 					</button>
 					<button className='mt-8 rounded-full bg-[#222222] py-4 px-4'>
 						<LinkedInLogoIcon height={26} width={26} color='#D3E97A'/> 
@@ -20,7 +20,7 @@ const Hero = () => {
 				</div>
 				
 			</div>
-			<img src={myProfile} alt="" className='rounded-2xl '/>
+			<img src={myProfile} alt="" className='rounded-2xl'/>
 		</div>
 	)
 }
