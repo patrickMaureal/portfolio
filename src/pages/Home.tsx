9c9799617583b5719540de4953319b486549152e
patrickMaureal <janpatrickmaureal@gmail.com>
2024-07-17 09:59:15 +0800

    add divider for sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,10 @@ import Hero from "../components/sections/hero/Hero"
 const Home = () => {
 
 	return (
-		<>
+		<div className="divide-y divide-gray-500 divide-y-2">
 			<Hero />
 			<About />
-		</>
+		</div>
 	)
 }
 
