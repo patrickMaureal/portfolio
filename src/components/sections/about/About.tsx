db49e4498b7167461a2b42cb95b942e9719429cb
patrickMaureal <janpatrickmaureal@gmail.com>
2024-07-23 14:01:17 +0800

    update about me page

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -2,9 +2,9 @@ import aboutImg from "../../../assets/profile/profile.png"
 
 const About = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-start p-8 md:px-10 md:py-12 md:space-x-20 lg:px-56 lg:py-10 lg:space-x-52 lg:space-y-0">
-			<div>
-				<h1 className="font-bebas font-normal md:text-7xl text-9xl text-white">About Me</h1>
+    <div className="flex flex-col md:flex-row justify-between items-start p-8 space-y-20 md:px-10 md:py-12 md:space-x-20 lg:px-56 lg:py-10 lg:space-x-52 lg:space-y-0">
+			<div className="hidden md:block">
+				<h1 className="font-bebas font-normal md:text-7xl text-9xl text-white ">About Me</h1>
 			</div>
 			<div className="flex flex-col space-y-6 self-center">
 				<p className="font-manrope text-subtitle text-lg font-normal">I am a front-end developer based in Sydney looking for exciting opportunities. Has Mechanical Engineering background. Likes to focus on accessibility when developing. </p>
