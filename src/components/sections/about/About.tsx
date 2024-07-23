16811a444cf6d39f7fae45b4d41a87692a0ffd8c
patrickMaureal <janpatrickmaureal@gmail.com>
2024-07-23 13:50:05 +0800

    update about me page

--- src/components/sections/about/About.tsx
+++ src/components/sections/about/About.tsx
@@ -2,12 +2,15 @@ import aboutImg from "../../../assets/profile/profile.png"
 
 const About = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-start p-8 md:px-40 md:py-12 lg:px-56 lg:py-10 space-y-20 lg:space-x-52 lg:space-y-0">
-			<div className="flex flex-col space-y-6">
-				<p className="font-manrope text-subtitle text-lg text-base">I am a front-end developer based in Sydney looking for exciting opportunities. Has Mechanical Engineering background. Likes to focus on accessibility when developing. </p>
+    <div className="flex flex-col md:flex-row justify-between items-start p-8 md:px-10 md:py-12 md:space-x-20 lg:px-56 lg:py-10 lg:space-x-52 lg:space-y-0">
+			<div>
+				<h1 className="font-bebas font-normal md:text-7xl text-9xl text-white">About Me</h1>
+			</div>
+			<div className="flex flex-col space-y-6 self-center">
+				<p className="font-manrope text-subtitle text-lg font-normal">I am a front-end developer based in Sydney looking for exciting opportunities. Has Mechanical Engineering background. Likes to focus on accessibility when developing. </p>
 				<a href="#" className="underline underline-offset-4 uppercase text-primary font-manrope text-sm"> More About Me</a>
 			</div>
-			<div>
+			<div className="md:hidden">
 				<img src={aboutImg} alt="" className="rounded-lg" />
 			</div>
 		</div>
