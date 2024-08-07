31281a2dd8e575f4d2da0e0f8a2c701463f26927
patrickMaureal <janpatrickmaureal@gmail.com>
2024-08-07 11:34:23 +0800

    add images

--- src/types/Projects.ts
+++ src/types/Projects.ts
@@ -1,3 +1,4 @@
+
 export type ProjectData = {
   Project: {
     projectImg: string;
@@ -17,7 +18,7 @@ export type ProjectData = {
 export const projects: ProjectData[] = [
 	{
     Project: {
-      projectImg: "src/assets/projects/portfolio.png",
+      projectImg: "public/assets/projects/portfolio.png",
       projectName: "Portfolio",
       projectDesc: "This project is a personal portfolio website designed to showcase my skills, projects, and professional experience. Developed using modern web technologies, the portfolio aims to provide a visually appealing and user-friendly interface to highlight my work and achievements.",
     },
@@ -32,7 +33,7 @@ export const projects: ProjectData[] = [
   },
   {
     Project: {
-      projectImg: "src/assets/projects/mao-tuburan.png",
+      projectImg: "public/assets/projects/mao-tuburan.png",
       projectName: "Integrated Asset Management System",
       projectDesc: "This project involves the development of a web-based portal for managing assets for an Agriculture Office. The portal is being developed as a freelance project for a student client as part of their capstone project. The primary goal of the portal is to streamline and enhance the management of agricultural assets, ensuring efficient tracking, utilization, and reporting of resources.",
     },
@@ -46,7 +47,7 @@ export const projects: ProjectData[] = [
   },
 	{
     Project: {
-      projectImg: "src/assets/projects/danao-wordpress.png",
+      projectImg: "public/assets/projects/danao-wordpress.png",
       projectName: "Danao City Landing Page",
       projectDesc: "The Danao City Landing Page is a web-based project designed to provide information and resources about Danao City. This project is developed using WordPress, aiming to deliver a user-friendly, informative, and visually appealing platform for residents and visitors.",
     },
@@ -60,7 +61,7 @@ export const projects: ProjectData[] = [
   },
 	{
     Project: {
-      projectImg: "src/assets/projects/islet.png",
+      projectImg: "public/assets/projects/islet.png",
       projectName: "ISLET Connect Portal",
       projectDesc: "The ISLET Connect Portal is a sophisticated web-based platform designed to streamline and enhance various administrative and operational processes within our organization. Leveraging the power and flexibility of the Laravel framework, this portal aims to provide a seamless user experience while ensuring robust functionality and security.",
     },
@@ -74,7 +75,7 @@ export const projects: ProjectData[] = [
   },
 	{
     Project: {
-      projectImg: "src/assets/projects/lakat.png",
+      projectImg: "public/assets/projects/lakat.png",
       projectName: "Lakat Booking System",
       projectDesc: "The Lakat Booking System is a web-based portal designed to manage and streamline the process of booking accommodations for a resort. This project is being developed as a freelance assignment for a student client as part of their Computer System Project. Utilizing the Laravel framework, the system aims to provide a user-friendly, efficient, and secure platform for both guests and resort staff.",
     },
