08a1356db05faebe09fb4d8d37af4405438c5556
patrickMaureal <janpatrickmaureal@gmail.com>
2024-08-13 16:31:44 +0800

    update contact layout

--- src/components/sections/contact/Contact.tsx
+++ src/components/sections/contact/Contact.tsx
@@ -3,8 +3,8 @@ import ContactForm from "../../ui/ContactForm"
 
 const Contact = () => {
 	return (
-		<div className="flex flex-col md:flex-row justify-between items-start p-8 md:px-20 md:py-12 md:space-x-20 md:space-y-0 lg:px-28 lg:py-10 space-y-20 lg:space-x-28 lg:space-y-0">
-			<div className="md:space-y-10">
+		<div className="flex flex-col md:flex-row justify-between items-start p-8 md:px-20 md:py-12 md:space-x-20 md:space-y-0 lg:px-28 lg:py-10 space-y-16 lg:space-x-28 lg:space-y-0">
+			<div className="space-y-10">
 				<div className="space-y-4 ">
 						<h1 className="font-bebas text-5xl font-normal text-white">
 						Let’s connect        
